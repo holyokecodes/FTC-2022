d51d151938bddec799d068563d68f34a1599ef05
zachattack531 <26945971+zachattack531@users.noreply.github.com>
2021-11-18 18:21:07 -0500

    Less code, more Adventure !
    Also fixed "Pesise Mode" to "Precise Mode"

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2022.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2022.java
@@ -16,17 +16,13 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
-import java.util.Comparator;
-import java.util.stream.Stream;
 import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import com.qualcomm.hardware.bosch.BNO055IMU;
-import com.qualcomm.robotcore.hardware.Blinker;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
@@ -45,57 +41,17 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  */
 @TeleOp(name = "Tele-op 2022 ")
 public class TeleOp2022 extends LinearOpMode {
-//    private Blinker expansion_Hub_2;
     private DcMotor backLeft;
     private DcMotor backRight;
     private DcMotor frontLeft;
     private DcMotor frontRight;
 
-//    private Encoder leftEncoder;
-//    private Encoder rightEncoder;
-//    private Encoder frontEncoder;
 
-    //private Servo finger;
-    //private DcMotorEx shooter;
-    //private DcMotor intake;
 
     private BNO055IMU imu;
     private ElapsedTime runtime = new ElapsedTime();
-    private final double sensitivity = 1;
 
-    static final double ServoIncrement   =  0.075; // amount to slew servo each CYCLE_MS cycle
-    // Maybe make this higher
-    static final int    CycleMS    = 50;     // period of each cycle
-    static final double MAX_POS     =  0.6;   // Maximum rotational position
-    static final double MIN_POS     =  0.3;   // Minimum rotational position
-    static final double MOTOR_INCREMENT   = 0.04;     // amount to ramp motor each CYCLE_MS cycle
-    static final int    CYCLE_MS    =   50;     // period of each cycle
-    static final double MAX_FWD     =  1.0;     // Maximum FWD power applied to motor
-    static final double MAX_REV     = -1.0;     // Maximum REV power applied to motor
 
-    // Define class members
-    double  power   = 0;
-    boolean rampUp  = true;
-
-    // Define class members
-    double  position = 0.3; // Starting position
-
-    float joystickDeadzone = 0.1f;
-
-    boolean shooterOn = false;
-    boolean shooterButton = false;
-    boolean shooterButtonBefore = false;
-
-    boolean intakeOn = false;
-    boolean intakeButton = false;
-    boolean intakeButtonBefore = false;
-
-    int fingerState = 1;
-
-    boolean fingerButton;
-    boolean fingerButtonBefore;
-
-    //    private double intakePower;
     @Override
     public void runOpMode() {
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
@@ -103,16 +59,9 @@ public class TeleOp2022 extends LinearOpMode {
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
 
-//        leftEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "frontRight"));
-//        rightEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "backLeft"));
-//        frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "frontLeft"));
-
-
         // Wait for the start button
         telemetry.addData(">", "Press Start to have a call to adventure.");
         telemetry.update();
-
-        //gamepad1.setJoystickDeadzone(joystickDeadzone); It doesn't exist in this version
 
         waitForStart();
 
@@ -140,9 +89,9 @@ public class TeleOp2022 extends LinearOpMode {
             double speedMultiplier = 1; //Multiplier for precision mode.
             if (gamepad1.right_trigger > 0.5) {
                 speedMultiplier = 0.5;
-                telemetry.addData("Pesise Mode", "On");
+                telemetry.addData("Precise Mode", "On");
             } else {
-                telemetry.addData("Pesise Mode", "Off");
+                telemetry.addData("Precise Mode", "Off");
             }
 
 
@@ -173,65 +122,6 @@ public class TeleOp2022 extends LinearOpMode {
             frontLeft.setPower(-frontLeftPower * speedMultiplier);
             frontRight.setPower(frontRightPower * speedMultiplier);
 
-            /* slew the servo, according to the rampUp (direction) variable
-            if (fingerState == 1) {
-                Keep stepping up until we hit the max value.
-                position += ServoIncrement;
-                if (position >= MAX_POS ) {
-                    position = MAX_POS;
-                    fingerState = 2;
-                }
-            }
-            else if (fingerState == 2) {
-
-                // Keep stepping down until we hit the min value.
-                position -= ServoIncrement;
-                if (position <= MIN_POS ) {
-                    position = MIN_POS;
-                    fingerState = 0;
-                }
-            }
-
-            if (fingerState == 0) telemetry.addData("Finger State", "Idle");
-            else if (fingerState == 1) telemetry.addData("Finger State","Extending...");
-            else if (fingerState == 2) telemetry.addData("Finger State","Retracting...");
-            else telemetry.addData("Finger State","[EERRRROORR]");
-
-            power += MOTOR_INCREMENT ;
-            if (power <= MAX_REV ) {
-                power = MAX_REV;
-                rampUp = !rampUp;  // Switch ramp direction
-            }
-            // if you hit the x button, invert the state of the shooter.
-            /*if (gamepad2.x){
-                shooterOn = !shooterOn;
-            }
-
-            if (gamepad1.left_trigger < 0.5f){
-                shooterOn = !shooterOn;
-            }*/
-            // Display the current values
-            telemetry.addData("Motor Power", "%5.2f", power);
-            telemetry.addData("Servo Position", "%5.2f", position);
-
-            /* Set the servo to the new position and pause;
-            finger.setPosition(position);
-
-            if (shooterOn){
-                shooter.setVelocity(6600);
-            } else {
-                shooter.setVelocity(0);
-            }
-            if (intakeOn){
-                intake.setPower(-power);
-            } else {
-                intake.setPower(0);
-            }
-            sleep(CycleMS);
-            idle();
-
-            telemetry.addData("Status", "Running");
-            // telemetry.addData("angle", angles.firstAngle);*/
             telemetry.update();
         }
 
