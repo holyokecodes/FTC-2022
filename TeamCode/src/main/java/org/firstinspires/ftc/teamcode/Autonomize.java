390e312d61e0679333eeac942fe069f2bb152196
zachattack531 <26945971+zachattack531@users.noreply.github.com>
2022-02-05 15:19:35 -0500

    Finished Autonomizing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomize.java
@@ -0,0 +1,66 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+import java.util.List;
+
+
+@Autonomous(name="Autonomous 2022 Red A")
+
+public class Autonomize extends LinearOpMode {
+    SampleMecanumDrive drivetrain;
+
+    private Pose2d start = new Pose2d(0, 0, Math.toRadians(180)); //Where the robot starts
+
+    private Pose2d shippingHub = new Pose2d(20, 20, Math.toRadians(180)); //Where the robot is after going to zone c
+
+    Trajectory shippingHubTraj;
+
+    @Override
+    public void runOpMode() {
+        drivetrain = new SampleMecanumDrive(hardwareMap);
+
+        shippingHubTraj = drivetrain.trajectoryBuilder(new Pose2d(-36, -60, Math.toRadians(90)))
+                .splineToConstantHeading(new Vector2d(-12, -40), Math.toRadians(90))
+                .build();
+
+
+        /**CReturnTrajectory = drivetrain.trajectoryBuilder(cZone)
+         .strafeTo(whiteLine)
+         .build();
+         */
+
+
+        /**     EndTrajectory = drivetrain.trajectoryBuilder(whiteLinePose)
+         .strafeTo(new Vector2d(55, 0))
+         .build();
+         */
+
+
+        /** Wait for the game to begin */
+        telemetry.addData(">", "Press Play to start op mode, wait for Charles III to see things though.");
+        telemetry.update();
+
+        waitForStart();
+
+        if (opModeIsActive()) {
+            // getUpdatedRecognitions() will return null if no new information is available since
+            // the last time that call was made.
+
+            drivetrain.followTrajectory(shippingHubTraj);
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -55,7 +55,7 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.04;//1.0 / rpmToVelocity(MAX_RPM);
+    public static double kV = 0.0278;//1.0 / rpmToVelocity(MAX_RPM);
     public static double kA = 0.006;
     public static double kStatic = 0;
 
@@ -89,7 +89,7 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 34.0713;//34.34074929639003;
     public static double MAX_ACCEL = 34.34074929639003;
-    public static double MAX_ANG_VEL = Math.toRadians(191.9590243902439);
+    public static double MAX_ANG_VEL = Math.toRadians(275.1491);//191.9590243902439);
     public static double MAX_ANG_ACCEL = Math.toRadians(191.9590243902439);
 
 
