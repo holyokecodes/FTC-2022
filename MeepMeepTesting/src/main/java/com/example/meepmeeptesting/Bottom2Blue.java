67dd07e2e655e7697af2c47dfd6b0023abc0e742
Alex <alexanderesolis@gmail.com>
2022-02-09 21:11:56 -0500

    Added MeepMeep Files

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/Bottom2Blue.java
@@ -0,0 +1,32 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
+
+public class Bottom2Blue {
+    public static void main(String[] args) {
+        // TODO: If you experience poor performance, enable this flag
+        // System.setProperty("sun.java2d.opengl", "true");
+
+        // Declare a MeepMeep instance
+        // With a field size of 800 pixels
+        MeepMeep mm = new MeepMeep(750)
+                // Set field image
+                .setBackground(MeepMeep.Background.FIELD_FREIGHT_FRENZY)
+                // Set theme
+                .setTheme(new ColorSchemeRedDark())
+                // Background opacity from 0-1
+                .setBackgroundAlpha(1f)
+                // Set constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-36, 60, Math.toRadians(-90)))
+                                .splineToConstantHeading(new Vector2d(-12, 40), Math.toRadians(-90))
+                                .build()
+                )
+                .start();
+    }
+}
